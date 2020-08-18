d7a17d76df4bf4d970d75d34e1b38ad65915ebc4
Hari Krishnan <harikrishnans3010@gmail.com>
2020-08-18 13:07:30 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{Component} from 'react';
 import './App.css';
+var async=require("async");
+// var fs=require('fs');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+var arr=[];
+var q = async.queue(function(task, callback) {
+  console.log('Number ' + task);
+  // fs.writeFile('file.txt',task);
+  setTimeout(function(){
+    callback();
+  },5000);
+}, 2);  //perform two tasks at a time
+
+q.drain = function() {
+  console.log('All items have been processed');
+};
+
+class App extends Component {
+  state={
+    value:0
+  }
+  submit = (e) => {
+    e.preventDefault();
+    arr.push(this.state.value);
+  }
+  update = (e) => {
+    this.setState({
+      value:e.target.value
+    });
+  }
+  emptyQueue = (e) => {
+    e.preventDefault();
+    arr.forEach(function(val) {
+      q.push(val, function() {
+        console.log('finished processing');
+      });
+    })
+  }
+  render() {
+    return (
+      <form>
+        <input type="text" onChange={this.update}></input><br/><br/>
+        <button onClick={this.submit}>Submit</button><br/><br/>
+        <button onClick={this.emptyQueue}>Empty Queue</button>
+      </form>
+    );
+  }
 }
-
 export default App;
