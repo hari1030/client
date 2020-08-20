6f11b1ad72687aa6dd3eabdc34c3326485a18cfe
Hari Krishnan <harikrishnans3010@gmail.com>
2020-08-20 12:49:11 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -4,31 +4,32 @@ import axios from 'axios';
 
 class App extends Component {
   state={
-    value:0
+    sender:null,
+    receiver:null,
+    message:null
   }
   submit = (e) => {
     e.preventDefault();
-    axios.post(`/${this.state.value}`).then((res)=>{
+    axios.post(`/${this.state.sender}`,{
+      sender:this.state.sender,
+      receiver:this.state.receiver,
+      message:this.state.message
+    }).then((res)=>{
       console.log(res.data);
     })
   }
   update = (e) => {
     this.setState({
-      value:e.target.value
+      [e.target.id]:e.target.value
     });
-  }
-  emptyQueue = (e) => {
-    e.preventDefault();
-    axios.post('/').then((res)=>{
-      console.log(res.data);
-    })
   }
   render() {
     return (
       <form>
-        <input type="text" onChange={this.update}></input><br/><br/>
+        <input type="text" id="sender" onChange={this.update}></input><br/><br/>
+        <input type="text" id="receiver" onChange={this.update}></input><br/><br/>
+        <input type="text" id="message" onChange={this.update}></input><br/><br/>
         <button onClick={this.submit}>Submit</button><br/><br/>
-        <button onClick={this.emptyQueue}>Empty Queue</button>
       </form>
     );
   }
